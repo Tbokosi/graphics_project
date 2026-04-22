8829c96cc24f15b3de07ae6e091db153884af264
cheehgovalah <bed-com-35-22@unima.ac.mw>
2026-04-22 23:09:51 +0300

    feature: graphics-object

---
+++ headers/GraphicsObject.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <string>
+using namespace std;
+using namespace glm;
+
+class GraphicsObject{
+    public:
+    virtual void Initialise(const string& shaderPath, const string& texturePath)=0;
+    virtual void Update(float deltaTime) =0;
+    virtual void Draw()=0;
+    virtual void CleanUp()=0;
+
+    virtual ~GraphicsObject(){}
+
+    protected:
+    mat4 m_transform;
+    unsigned int m_VBO;
+    unsigned int M_VAO;
+};
+  
